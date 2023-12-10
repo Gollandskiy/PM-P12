338c4c806c0900f08c2d304b6817c155279b4477
nikita.prigolovkin <vitalii.prigolovkin@gmail.com>
2023-12-10 21:30:01 +0300

    update 10.12.2023

--- App/Helper.cs
+++ App/Helper.cs
@@ -8,6 +8,39 @@ namespace App
 {
     public class Helper
     {
+        public String CombineUrl(params String[] parts)
+        {
+            if (parts is null) { throw new NullReferenceException("Parts is null"); }
+            if (parts.Length == 0) { throw new ArgumentException("Parts is empty"); }
+
+            StringBuilder result = new();
+            string temp;
+            bool wasNull = false;
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (parts[i] is null)
+                {
+                    wasNull = true;
+                    continue;
+                }
+                if (wasNull)
+                {
+                    throw new ArgumentException("Non-Null argument after Null one");
+                }
+
+                if (parts[i] == "..") { continue; }
+                temp = "/" + parts[i].TrimStart('/').TrimEnd('/');
+
+                if ((i != parts.Length - 1) && parts[i + 1] == "..") { continue; }
+                result.Append(temp);
+            }
+            if (result.Length == 0)
+            {
+                throw new ArgumentException("All arguments are null");
+            }
+            return result.ToString();
+
+        }
         static char[] chars = { '!', '?', '.', ',' };
         public string Ellipsis(string input, int len)
         {
--- Test1/UnitTest1.cs
+++ Test1/UnitTest1.cs
@@ -23,5 +23,100 @@ namespace UnitTest
                 "Hello, friend.",
                 helper.Finalize("Hello, friend."));
         }
+        [TestMethod]
+        public void CombineUrlTest()
+        {
+            Helper helper = new();
+
+            Dictionary<string[], string> testCases = new()
+            {
+                { new[] { "/home", "index" }, "/home/index" },
+                { new[] { "/shop", "/cart" }, "/shop/cart" },
+                { new[] { "auth/", "logout" }, "/auth/logout" },
+                { new[] { "forum/", "topic/" }, "/forum/topic" },
+                { new[] { "/home///", "index" }, "/home/index" },
+                { new[] { "///home/", "/index" }, "/home/index" },
+                { new[] { "home/", "////index" }, "/home/index" },
+                { new[] { "///home/////", "////index///" }, "/home/index" },
+            };
+            foreach (var testCase in testCases)
+            {
+                Assert.AreEqual(
+                    testCase.Value,
+                    helper.CombineUrl(testCase.Key[0], testCase.Key[1]),
+                    $"{testCase.Key[0]} - {testCase.Key[1]}"
+                );
+            }
+        }
+        [TestMethod]
+        public void CombineUrlMultiTest()
+        {
+            Helper helper = new();
+            Dictionary<String[], String> testCases = new()
+            {
+                 { new[] { "/home", "/index", "/gmail" }, "/home/index/gmail" },
+                { new[] { "/shop", "/cart/", "com" }, "/shop/cart/com" },
+                { new[] { "auth/", "logout" }, "/auth/logout" },
+                { new[] { "forum", "topic/", "/com/" }, "/forum/topic/com" },
+                { new[] { "//forum////", "topic////", "///com" }, "/forum/topic/com" },
+                { new[] { "forum", "topic", "com" }, "/forum/topic/com" },
+                { new[] { "/forum/", "/topic///////////", "//com////////////////" }, "/forum/topic/com" },
+                { new[] { "/shop", "/cart", "/user", "..", "/123" }, "/shop/cart/123" },
+                { new[] { "/shop///", "///cart", "user", "..", "////123///" }, "/shop/cart/123" },
+                { new[] { "/shop///", "///cart", "user", "..", "////123///", "456" }, "/shop/cart/123/456" },
+                { new[] { "/shop///", "///cart", "..", "user//", "///123", "456//" }, "/shop/user/123/456" },
+            };
+            foreach (var testCase in testCases)
+            {
+                Assert.AreEqual(
+                    testCase.Value,
+                    helper.CombineUrl(testCase.Key),
+                    $"{testCase.Value} -- {testCase.Key[0]} + {testCase.Key[1]}"
+                );
+            }
+        }
+        [TestMethod]
+        public void CombineUrlExceptionTest()
+        {
+            Helper helper = new();
+            Assert.AreEqual("/home", helper.CombineUrl("/home", null!));
+            Assert.AreEqual("/home/path", helper.CombineUrl("/home", "///path//", null!));
+            Assert.AreEqual("/home/user", helper.CombineUrl("/home", "///path//", "..", "user//", null!));
+
+            var ex = Assert.ThrowsException<ArgumentException>(
+                () => helper.CombineUrl(null!, null!)
+            );
+            Assert.AreEqual("All arguments are null", ex.Message);
+
+            ex = Assert.ThrowsException<ArgumentException>(
+                () => helper.CombineUrl(null!, null!, null!, null!, null!, null!)
+            );
+            Assert.AreEqual("All arguments are null", ex.Message);
+
+            ex = Assert.ThrowsException<ArgumentException>(
+                () => helper.CombineUrl()
+            );
+            Assert.AreEqual("Parts is empty", ex.Message);
+
+            var ex2 = Assert.ThrowsException<NullReferenceException>(
+                () => helper.CombineUrl(null!)
+            );
+            Assert.AreEqual("Parts is null", ex2.Message);
+
+            var ex3 = Assert.ThrowsException<ArgumentException>(
+                () => helper.CombineUrl(null!, "/subsection")
+            );
+            Assert.AreEqual("Non-Null argument after Null one", ex3.Message);
+
+            ex3 = Assert.ThrowsException<ArgumentException>(
+                () => helper.CombineUrl("/path", null!, "/subsection")
+            );
+            Assert.AreEqual("Non-Null argument after Null one", ex3.Message);
+
+            ex3 = Assert.ThrowsException<ArgumentException>(
+                () => helper.CombineUrl("/path", "/path2", null!, "/subsection")
+            );
+            Assert.AreEqual("Non-Null argument after Null one", ex3.Message);
+        }
     }
 }
